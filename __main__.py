2b92327306309c0d47a25f5117938b3a5a6829a1
Michael C <mdcovarrubias@gmail.com>
2020-01-30 13:57:37 -0800

    First pass to generate the STFT of the data files.

--- __main__.py
+++ __main__.py
@@ -1,31 +1,64 @@
 #!/usr/bin/env python3
 
+# imports to make spectrogram images
+from scipy import signal
+from scipy.fft import fftshift
+import matplotlib.pyplot as plt
+
+
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
 import numpy as np # linear algebra
 import os # accessing directory structure
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import scipy.io
+
 KEYS = ['id', 'tag', 'nS', 'sampFreq', 'marker', 'timestamp', 'data', 'trials']
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+# constant representing directory path to data files
+DATA_FILES_PATH = os.path.join(CWD, 'data')
+
+"""
+data is a 25x(# ofDataPoints)
+
+we want rows 4-17
+thus (4-17)x(:)
+"""
+
+
+def get_data_files():
+    for dirname, _, filenames in os.walk('/kaggle/input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
 
 def main():
     """
     Main Entrance of program
     :return None:
     """
-    # pd.set_option('display.max_rows', None)
-    # pd.set_option('display.max_columns', None)
-    # pd.set_option('display.width', None)
-    # pd.set_option('display.max_colwidth', -1)
+    print(CWD)
 
-    raw_file = scipy.io.loadmat('EEGData/eeg_record1.mat')
+    raw_file = scipy.io.loadmat('{0}/data/eeg_record30.mat'.format(CWD))
     obj = raw_file['o']
 
-    data = pd.DataFrame.from_dict(raw_file["o"]["data"][0,0])
+    data_unclean = pd.DataFrame.from_dict(raw_file["o"]["data"][0,0])
+    data = pd.DataFrame(data_unclean).to_numpy()
 
-    if data:
-        print('yay')
+    fs = 128
+    amp = 2
+    f, t, Zxx = signal.stft(data[:, 3], fs, window='blackman', nperseg=1920)
+    plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=amp)
+    plt.title('STFT Magnitude')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+    # time variable = [1, 2, ... 308868]
+    time = [item for item in range(1, np.size(data, 0) + 1)]
+
 
 if __name__ == '__main__':
     main()
