7dabb2ab9bb9ffd027c7510f4ce1a4e2626c5076
Michael C <mdcovarrubias@gmail.com>
2020-01-30 15:06:52 -0800

    Add function to create stft png files for each input data file.

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@
 from scipy import signal        # imports to make spectrogram images
 import matplotlib.pyplot as plt
 
+import shutil
 import numpy as np
 import os
 import pandas as pd             # data processing
@@ -12,23 +13,17 @@ KEYS = ['id', 'tag', 'nS', 'sampFreq', 'marker', 'timestamp', 'data', 'trials']
 CWD = os.path.dirname(os.path.realpath(__file__))
 
 DATA_FILES_PATH = os.path.join(CWD, 'data') # constant representing directory path to data files
+OUTPUT_PATH = os.path.join(CWD, 'output')   # constant representing directory of generated files
 FREQUENCY = 128                             # frequency rate is 128Hz
 M = 1920                                    # M = frequency * delta_time = 128 Hz * 15 seconds
 MAX_AMP = 2                                 # Max amplitude for short-time fourier transform graph
-
-"""
-data is a 25x(# ofDataPoints)
-
-we want rows 4-17
-thus (4-17)x(:)
-"""
 
 
 def get_all_data_files():
     """
     Function used to get string values of all files in a directory e.g.
-    /create-spectrograms/data/eeg_record1.mat,
-    /create-spectrograms/data/eeg_record2.mat, etc.
+    '/create-spectrograms/data/eeg_record1.mat',
+    '/create-spectrograms/data/eeg_record2.mat', etc.
     :return all_files: list of string values of all files in a directory
     """
     all_files = []
@@ -56,7 +51,7 @@ def load_data_from_file(path_to_file):
     return data
 
 
-def generate_stft_from_data(channel, fs, m, max_amp, data):
+def generate_stft_from_data(channel, fs, m, max_amp, data, output_filepath):
     """
     Function used to generate the Fast-Time Fourier Transform (stft) from data
     :param channel: which channel of the data we are analyzing. Integer value between 0 - 24
@@ -64,14 +59,32 @@ def generate_stft_from_data(channel, fs, m, max_amp, data):
     :param m: total number of points in window e.g. 1920
     :param max_amp: max amplitude for stft plot
     :param data: complete dataset from input file
+    :param output_filepath: path to export file of short time fourier transform plot of data
     :return None:
     """
     f, t, Zxx = signal.stft(data[:, channel], fs, window='blackman', nperseg=m)
+
     plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=max_amp)
     plt.title('STFT Magnitude')
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
-    plt.show()
+
+    plt.savefig(output_filepath)
+
+
+def create_output_directory(output_path):
+    """
+    Function used to create the output directory for Short-Time Fourier Transform
+    images created for all input files and each channel of an input file.
+    if output directory exists, we delete it and recreate it.
+    :param output_path: path of the output files we want to create e.g. './output'
+    :return None:
+    """
+
+    if os.path.isdir(output_path):
+        shutil.rmtree(output_path, ignore_errors=True)
+
+    os.mkdir(output_path)
 
 
 def main():
@@ -82,15 +95,16 @@ def main():
 
     all_files = get_all_data_files()
 
+    create_output_directory(OUTPUT_PATH)
+
     for data_file in all_files:
 
         data = load_data_from_file(data_file)
-        generate_stft_from_data(3, FREQUENCY, M, MAX_AMP, data)
-        print(data_file)
-        break
+        output_basename = os.path.basename(data_file)
+        output_basename = output_basename.split('.')[0]
+        output_filepath = os.path.join(CWD, 'output', output_basename)
 
-    # time variable = [1, 2, ... 308868]
-    time = [item for item in range(1, np.size(data, 0) + 1)]
+        generate_stft_from_data(3, FREQUENCY, M, MAX_AMP, data, output_filepath)
 
 
 if __name__ == '__main__':
